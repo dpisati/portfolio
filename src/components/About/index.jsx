1ecf2fe2e57a75214c3fbc9ed6b08b6edf0d30fd
dpisati <43629737+dpisati@users.noreply.github.com>
2021-08-24 21:43:53 +1200

    blank links fixed

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -18,22 +18,22 @@ export default function About() {
                 </p>
 
                 <div className={styles.contactsContainer}>
-                    <a href="https://www.facebook.com/daniel.pisati" target="_blank">
+                    <a href="https://www.facebook.com/daniel.pisati" target="_blank" rel="noreferrer">
                         <div className={`${styles.contact} ${styles.facebook}`}>
                             <img src="/assets/icons/facebook.svg" alt="facebook" />
                         </div>
                     </a>
-                    <a href="https://www.linkedin.com/in/daniel-pisati/" target="_blank">
+                    <a href="https://www.linkedin.com/in/daniel-pisati/" target="_blank" rel="noreferrer">
                         <div className={`${styles.contact} ${styles.linkedin}`}>
                             <img src="/assets/icons/linkedin.svg" alt="linkedin" />
                         </div>
                     </a>
-                    <a href="https://github.com/dpisati" target="_blank">
+                    <a href="https://github.com/dpisati" target="_blank" rel="noreferrer">
                         <div className={`${styles.contact} ${styles.github}`}>
                             <img src="/assets/icons/github.svg" alt="github" />
                         </div>
                     </a>
-                    <a href="mailto:dpisati@gmail.com" target="_blank">                        
+                    <a href="mailto:dpisati@gmail.com">
                         <div className={`${styles.contact} ${styles.email}`}>
                             <img src="/assets/icons/email.svg" alt="email" />
                         </div>
