a55dd496eeb833d7bfbf0bd027ef00a8d15516af
dpisati <43629737+dpisati@users.noreply.github.com>
2021-08-25 12:46:04 +1200

    project page responsive

--- src/components/ProjectPage/index.jsx
+++ src/components/ProjectPage/index.jsx
@@ -1,21 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import styles from './styles.module.css';
 
 export default function ProjectPage({ project }) {
+
+    // const [color, setColor] = useState('');
+
+    // function hexToRgbA(hex){
+    //     var c;
+    //     if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)){
+    //         c= hex.substring(1).split('');
+    //         if(c.length== 3){
+    //             c= [c[0], c[0], c[1], c[1], c[2], c[2]];
+    //         }
+    //         c= '0x'+c.join('');
+    //         return 'rgba('+[(c>>16)&255, (c>>8)&255, c&255].join(',')+', .2';
+    //     }
+    //     throw new Error('Bad Hex');
+    // }
+    
+    
+    // useEffect(() => {
+    //     setColor(hexToRgbA(project.color));
+    // }, [])
+
+
     return (
         <main className={styles.projectContainer}>
-            <div className={styles.projectContent}>
+            <div 
+                className={styles.projectContent} 
+                style= {{ border: ` 1px solid ${project.color}` }}
+            >
+                <spam style= {{ background: project.color }}></spam>
                 <header>
                     <div className={styles.projectTitle}>
                         <h1>{project.title}</h1>
                         <h2>{project.slogan}</h2>
                     </div>
+                    
+                    
                     <aside>
-                        <h1>A</h1>
-                        <h1>B</h1>
-                        <h1>C</h1>
-                        <h1>D</h1>
+                        <p>Tools used in this project</p>
+
+                        <div className={styles.toolsUsedcontainer}>
+
+                            {project.tools.map(tool => {
+                                return (
+                                    <div className={styles.tool}>
+                                        <img src={tool.icon} alt={tool.name} />
+                                        <p>{tool.name}</p>
+                                    </div>
+                                )
+                            })}
+
+                        </div>
+                        
                     </aside>
                 </header>
 
@@ -24,9 +63,28 @@ export default function ProjectPage({ project }) {
 
                     <div className={styles.projectDescription}>
                         <p>{project.description}</p>
-                        <div className={styles.links}>
-                            <button>Demo</button>
-                            <button>Github</button>
+                        <div className={`
+                            ${styles.links} 
+                            ${project.demo && project.github ? '' : styles.single}
+                        `}>
+                            {project.demo && (
+                                <a 
+                                    href={project.demo} 
+                                    target="_blank" 
+                                    rel="noreferrer"
+                                >
+                                    Demo
+                                </a>
+                            )}
+                            {project.github && (
+                                <a 
+                                    href="" 
+                                    target="_blank" 
+                                    rel="noreferrer"
+                                >
+                                    Github
+                                </a>
+                            )}
                         </div>
                     </div>
                 </div>
--- src/lib/data.js
+++ src/lib/data.js
@@ -1,32 +1,69 @@
 export const projects = [
     {
         title: 'MiXR',
-        slogan: 'AR in your hands',
-        img: '/assets/images/ecollect.png',
-        color: '#8F53FF',
-        slug: 'project-1',
-        description: 'Lorem ipsum dolor sit amet',
-        date: '27-04-2021',
+        // slogan: 'AR in your hands',
+        slogan: 'Make everything come to life',
+        img: '/assets/images/mixr.png',
+        color: '#7E41A5',
+        demo: 'https://mixr.tripod-digital.com/',
+        slug: 'mixr',
+        description: 'MiXR is a web application that simplifies the process of creating augmented reality in a quick and simple way. Just uploading the desirable content and choosing a few easy configurations, any user can create their own AR.',
         tools: [
             {
                 name: 'React',
-                icon: 'aaa'
+                icon: '/assets/icons/react.png'
+            },
+            {
+                name: 'CSS',
+                icon: '/assets/icons/css.png'
+            },
+            {
+                name: 'Firebase',
+                icon: '/assets/icons/firebase.png'
+            },
+            {
+                name: 'Figma',
+                icon: '/assets/icons/figma.png'
+            },
+            {
+                name: 'Whimsical',
+                icon: '/assets/icons/whimsical.png'
+            },
+            {
+                name: 'Photoshop',
+                icon: '/assets/icons/photoshop.png'
             }
         ]
 
     },
     {
         title: 'Motorly',
-        slogan: 'Find card in a snap',
+        slogan: 'Find cars in a snap',
         img: '/assets/images/motorly.png',
         color: '#FF0042',
-        slug: 'project-2',
-        description: 'Lorem ipsum dolor sit amet',
-        date: '27-04-2021',
+        demo: 'https://motorly.nz/',
+        slug: 'motorly',
+        description: 'Motorly is a mobile application that helps to find cars using their number plate or uploading a photo. It looks for offers on TradeMe and Facebook and gives the lowest, highest, and average prices. Also, a few useful information about the requested car like model, engine, year, etc.',
         tools: [
             {
-                name: 'Vue',
-                icon: 'aaa'
+                name: 'React',
+                icon: '/assets/icons/react.png'
+            },
+            {
+                name: 'Next',
+                icon: '/assets/icons/next.png'
+            },
+            {
+                name: 'CSS',
+                icon: '/assets/icons/css.png'
+            },
+            {
+                name: 'Firebase',
+                icon: '/assets/icons/firebase.png'
+            },
+            {
+                name: 'Figma',
+                icon: '/assets/icons/figma.png'
             }
         ]
 
@@ -34,7 +71,7 @@ export const projects = [
     {
         title: 'Proffy',
         slogan: 'Find teachers online',
-        img: '/assets/images/pokedex_app.png',
+        img: '/assets/images/pokedex.png',
         color: '#487BFF',
         slug: 'project-3',
         description: 'Lorem ipsum dolor sit amet',
@@ -50,7 +87,7 @@ export const projects = [
     {
         title: 'Happy',
         slogan: 'Find orphanages nearby',
-        img: '/assets/images/happy_app.png',
+        img: '/assets/images/happy.png',
         color: '#0FC1CB',
         slug: 'project-3',
         description: 'Lorem ipsum dolor sit amet',
