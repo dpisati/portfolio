8889977077d6ca5dd95c4b01fa4fe476df4038cc
dpisati <43629737+dpisati@users.noreply.github.com>
2021-08-26 16:50:57 +1200

    share png

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,14 +40,14 @@ export default function Home() {
         <meta property="og:url" content="https://dpportfolio.vercel.app/" />
         <meta property="og:title" content="Daniel Pisati - Designer / Developer" />
         <meta property="og:description" content="Hi, I'm Daniel Pisati, a UX / UI Designer and frontend developer 🚀" />
-        <meta property="og:image" content="%PUBLIC_URL%/share.png"/>
+        <meta property="og:image" content="/share.png"/>
 
         
         <meta property="twitter:card" content="summary_large_image"/>
         <meta property="twitter:url" content="https://dpportfolio.vercel.app/"/>
         <meta property="twitter:title" content="Daniel Pisati - Designer / Developer"/>
         <meta property="twitter:description" content="Hi, I'm Daniel Pisati, a UX / UI Designer and frontend developer 🚀" />
-        <meta property="twitter:image" content="%PUBLIC_URL%/share.png" />
+        <meta property="twitter:image" content="/share.png" />
       </Head>
 
       <Header isLandingPage={true}/>
