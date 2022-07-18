5744c07338cb46ddfb96aad12fea49284e17955b
dpisati <43629737+dpisati@users.noreply.github.com>
2022-07-18 16:39:15 +1200

    change li to div tags

--- src/components/AnimatedBackground/index.js
+++ src/components/AnimatedBackground/index.js
@@ -1,5 +1,4 @@
-import { useTheme } from 'next-themes';
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import styles from './styles.module.css';
 
@@ -7,18 +6,18 @@ export default function AnimatedBackground() {
     return (
         <div className={styles.area}>
             <div className={styles.circles}>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
             </div>
         </div>
     );
