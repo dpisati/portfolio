03cec76613f682ce987a044214c2be94846879e9
Daniel <daniel.pisati@phocassoftware.com>
2022-07-24 00:39:55 +1200

    about text

--- src/components/ProjectPage/index.tsx
+++ src/components/ProjectPage/index.tsx
@@ -1,184 +1,168 @@
-import React, { useEffect, useState } from 'react';
-import styles from './styles.module.css';
-import { motion } from 'framer-motion';
-import { fadeInUp, container, fadeInRight } from '../../utils/animations.js';
-import Image from 'next/image';
-import { IProject } from '../../pages/projects/[slug]';
+import React, { useEffect, useState } from "react";
+import styles from "./styles.module.css";
+import { motion } from "framer-motion";
+import { fadeInUp, container, fadeInRight } from "../../utils/animations";
+import Image from "next/image";
+import { IProject } from "../../pages/projects/[slug]";
 
 export default function ProjectPage({ project }: { project: IProject }) {
-    const [showFigma, setShowFigma] = useState(false);
+  const [showFigma, setShowFigma] = useState(false);
 
-    useEffect(() => {
-        setTimeout(() => {
-            setShowFigma(true);
-        }, 1000);
-    }, []);
+  useEffect(() => {
+    setTimeout(() => {
+      setShowFigma(true);
+    }, 1000);
+  }, []);
 
-    return (
-        <main className={styles.projectContainer}>
-            <div
-                className={styles.projectContent}
-                style={{ border: ` 1px solid ${project.color}` }}
+  return (
+    <main className={styles.projectContainer}>
+      <div
+        className={styles.projectContent}
+        style={{ border: ` 1px solid ${project.color}` }}
+      >
+        <span
+          style={{
+            background: project.color,
+          }}
+        ></span>
+        <header>
+          <motion.div
+            initial="initial"
+            variants={container}
+            animate="animate"
+            className={styles.projectTitle}
+          >
+            <motion.h1 initial="initial" variants={fadeInUp} animate="animate">
+              {project.title}
+            </motion.h1>
+            <motion.h2 initial="initial" variants={fadeInUp} animate="animate">
+              {project.slogan}
+            </motion.h2>
+          </motion.div>
+
+          <motion.aside
+            initial="initial"
+            variants={container}
+            animate="animate"
+          >
+            <motion.p initial="initial" variants={fadeInUp} animate="animate">
+              Tools used in this project
+            </motion.p>
+
+            <motion.div
+              initial="initial"
+              variants={container}
+              animate="animate"
+              className={styles.toolsUsedcontainer}
             >
-                <span
-                    style={{
-                        background: project.color,
-                    }}
-                ></span>
-                <header>
-                    <motion.div
-                        initial="initial"
-                        variants={container}
-                        animate="animate"
-                        className={styles.projectTitle}
-                    >
-                        <motion.h1
-                            initial="initial"
-                            variants={fadeInUp}
-                            animate="animate"
-                        >
-                            {project.title}
-                        </motion.h1>
-                        <motion.h2
-                            initial="initial"
-                            variants={fadeInUp}
-                            animate="animate"
-                        >
-                            {project.slogan}
-                        </motion.h2>
-                    </motion.div>
-
-                    <motion.aside
-                        initial="initial"
-                        variants={container}
-                        animate="animate"
-                    >
-                        <motion.p
-                            initial="initial"
-                            variants={fadeInUp}
-                            animate="animate"
-                        >
-                            Tools used in this project
-                        </motion.p>
-
-                        <motion.div
-                            initial="initial"
-                            variants={container}
-                            animate="animate"
-                            className={styles.toolsUsedcontainer}
-                        >
-                            {project.tools.map((tool) => {
-                                return (
-                                    <motion.div
-                                        variants={fadeInUp}
-                                        key={tool.name}
-                                        className={styles.tool}
-                                    >
-                                        <Image
-                                            src={tool.icon}
-                                            alt={tool.name}
-                                            width={45}
-                                            height={45}
-                                            priority={true}
-                                            layout="responsive"
-                                        />
-                                        <p>{tool.name}</p>
-                                    </motion.div>
-                                );
-                            })}
-                        </motion.div>
-                    </motion.aside>
-                </header>
-
-                <div className={styles.projectBody}>
-                    <motion.img
-                        initial="initial"
-                        variants={fadeInRight}
-                        animate="animate"
-                        src={project.img}
-                        alt={project.title}
+              {project.tools.map((tool) => {
+                return (
+                  <motion.div
+                    variants={fadeInUp}
+                    key={tool.name}
+                    className={styles.tool}
+                  >
+                    <Image
+                      src={tool.icon}
+                      alt={tool.name}
+                      width={45}
+                      height={45}
+                      priority={true}
+                      layout="intrinsic"
                     />
+                    <p>{tool.name}</p>
+                  </motion.div>
+                );
+              })}
+            </motion.div>
+          </motion.aside>
+        </header>
 
-                    <div className={styles.projectDescription}>
-                        <motion.p
-                            initial="initial"
-                            variants={fadeInUp}
-                            animate="animate"
-                        >
-                            {project.description}
-                        </motion.p>
-                        <motion.div
-                            initial="initial"
-                            variants={container}
-                            animate="animate"
-                            className={`
+        <div className={styles.projectBody}>
+          <motion.img
+            initial="initial"
+            variants={fadeInRight}
+            animate="animate"
+            src={project.img}
+            alt={project.title}
+          />
+
+          <div className={styles.projectDescription}>
+            <motion.p initial="initial" variants={fadeInUp} animate="animate">
+              {project.description}
+            </motion.p>
+            <motion.div
+              initial="initial"
+              variants={container}
+              animate="animate"
+              className={`
                                 ${styles.links} 
                                 ${
-                                    project.demo && project.github
-                                        ? ''
-                                        : styles.single
+                                  project.demo && project.github
+                                    ? ""
+                                    : styles.single
                                 }
                             `}
-                        >
-                            {project.demo && (
-                                <motion.a
-                                    initial="initial"
-                                    variants={fadeInUp}
-                                    animate="animate"
-                                    href={project.demo}
-                                    target="_blank"
-                                    rel="noreferrer"
-                                    className={styles.visit}
-                                    style={{ background: project.color }}
-                                >
-                                    Visit
-                                </motion.a>
-                            )}
-                            {project.github && (
-                                <motion.a
-                                    initial="initial"
-                                    variants={fadeInUp}
-                                    animate="animate"
-                                    href={project.github}
-                                    className={styles.github}
-                                    target="_blank"
-                                    rel="noreferrer"
-                                >
-                                    Github
-                                </motion.a>
-                            )}
-                        </motion.div>
-                    </div>
-                </div>
+            >
+              {project.demo && (
+                <motion.a
+                  initial="initial"
+                  variants={fadeInUp}
+                  animate="animate"
+                  href={project.demo}
+                  target="_blank"
+                  rel="noreferrer"
+                  className={styles.visit}
+                  style={{ background: project.color }}
+                >
+                  Visit
+                </motion.a>
+              )}
+              {project.github && (
+                <motion.a
+                  initial="initial"
+                  variants={fadeInUp}
+                  animate="animate"
+                  href={project.github}
+                  className={styles.github}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Github
+                </motion.a>
+              )}
+            </motion.div>
+          </div>
+        </div>
 
-                {project.design && (
-                    <div className={styles.designProcessContainer}>
-                        <hr />
-                        <h3>{project.design.title}</h3>
-                        <h4>The role</h4>
-                        <p>{project.design.description}</p>
+        {project.design && (
+          <div className={styles.designProcessContainer}>
+            <hr />
+            <h3>{project.design.title}</h3>
+            <h4>The role</h4>
+            <p>{project.design.description}</p>
 
-                        <h4>Challenges</h4>
-                        <p>{project.design.challenges}</p>
+            <h4>Challenges</h4>
+            <p>{project.design.challenges}</p>
 
-                        <h4>Solution</h4>
-                        <p>{project.design.solution}</p>
+            <h4>Solution</h4>
+            <p>{project.design.solution}</p>
 
-                        <hr />
-                        <h4 className={styles.designPreview}>Design Preview</h4>
-                        <div className={styles.iframeContainer}>
-                            {showFigma && (
-                                <iframe
-                                    scrolling="no"
-                                    frameBorder="0"
-                                    src={project.design.iframe}
-                                    allowFullScreen
-                                ></iframe>
-                            )}
-                        </div>
-                    </div>
-                )}
+            <hr />
+            <h4 className={styles.designPreview}>Design Preview</h4>
+            <div className={styles.iframeContainer}>
+              {showFigma && (
+                <iframe
+                  scrolling="no"
+                  frameBorder="0"
+                  src={project.design.iframe}
+                  allowFullScreen
+                ></iframe>
+              )}
             </div>
-        </main>
-    );
+          </div>
+        )}
+      </div>
+    </main>
+  );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,18 @@
-import './global.css';
-import { AnimatePresence } from 'framer-motion';
-import { ThemeProvider } from 'next-themes';
-import AnimatedBackground from '../components/AnimatedBackground';
-import { AppProps } from 'next/app';
+import "../styles/global.css";
+import { AnimatePresence } from "framer-motion";
+import { ThemeProvider } from "next-themes";
+import AnimatedBackground from "../components/AnimatedBackground";
+import { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return (
-        <ThemeProvider>
-            <AnimatePresence exitBeforeEnter={true}>
-                <AnimatedBackground />
-                <Component {...pageProps} />
-            </AnimatePresence>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider>
+      <AnimatePresence exitBeforeEnter={true}>
+        <AnimatedBackground />
+        <Component {...pageProps} />
+      </AnimatePresence>
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
